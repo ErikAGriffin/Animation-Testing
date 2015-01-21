b6f0fc4c0dc06fe3625b22e70a13b7a85ceb661c
Erik Griffin <griffin.erik@gmail.com>
2015-01-22 00:31:40 +0100

    Added an AlertTestView

---
+++ AlertTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlertTestViewController.h
+//  Animation
+//
+//  Created by Erik Griffin on 1/22/15.
+//  Copyright (c) 2015 Erik Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlertTestViewController : UIViewController
+
+@end
---
+++ AlertTestViewController.m
@@ -0,0 +1,40 @@
+//
+//  AlertTestViewController.m
+//  Animation
+//
+//  Created by Erik Griffin on 1/22/15.
+//  Copyright (c) 2015 Erik Griffin. All rights reserved.
+//
+
+#import "AlertTestViewController.h"
+
+@interface AlertTestViewController ()
+
+@end
+
+@implementation AlertTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
